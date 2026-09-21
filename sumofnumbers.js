dad6148b8b881c381c7856d144f77cd5b354eb01
Tyler Gabriel Lasquero <tylerlasquero2@gmail.com>
2026-09-20 22:32:00 -1000

    Implement sum of numbers functions in TypeScript

---
+++ sumofnumbers.js
@@ -0,0 +1,30 @@
+"use strict";
+function sumFor(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+function sumWhile(numbers) {
+    let sum = 0;
+    let i = 0;
+    while (i < numbers.length) {
+        sum += numbers[i];
+        i++;
+    }
+    return sum;
+}
+function sumRecursion(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+    return numbers[0] + sumRecursion(numbers.slice(1));
+}
+function sumTheFunctionalWay(numbers) {
+    return numbers.reduce((sum, number) => sum + number, 0);
+}
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
